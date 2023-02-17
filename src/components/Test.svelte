c231a810c8fd8fe29dc52f0d35e324dcea3cb881
Nick <nickfriedrich@outlook.com>
2023-02-17 13:10:47 +0100

    imported a test svelte component

---
+++ src/components/Test.svelte
@@ -0,0 +1,13 @@
+<script> 
+   	import { onMount } from 'svelte';
+       let onmountstring = "empty"
+       let string = `this string contains some <strong>HTML!!!</strong>`;
+    
+       onMount(async () => {
+        onmountstring = "onMount has been executed"
+    });
+
+</script>
+
+<p>{@html string}</p>
+<p>{ onmountstring }</p>
